83d9e9104000cea9169affd3713df720e3829060
rebibabo <2841329980@qq.com>
2024-09-15 10:34:03 +0800

    change

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 pip install --upgrade pip
-pip install python-dotenv xformers loguru
+pip install python-dotenv xformers loguru openai
 pip install "unsloth[cu118-torch240] @ git+https://github.com/unslothai/unsloth.git"
