c6f0de7c8f7f459a502b9f03d41c6f0b8c9de847
rebibabo <2841329980@qq.com>
2024-09-15 10:36:22 +0800

    add setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 pip install --upgrade pip
-pip install python-dotenv xformers loguru openai
+pip install python-dotenv xformers loguru openai rouge_chinese jieba
 pip install "unsloth[cu118-torch240] @ git+https://github.com/unslothai/unsloth.git"
