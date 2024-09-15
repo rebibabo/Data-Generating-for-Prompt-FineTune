47a89bb7ce756e5376c46627c716feb8ed09baef
rebibabo <2841329980@qq.com>
2024-09-15 12:06:40 +0800

    change finetune way

--- evaluate.py
+++ evaluate.py
@@ -1,19 +1,9 @@
 from dotenv import load_dotenv
 load_dotenv()
 from unsloth import FastLanguageModel
+from transformers import TextStreamer
 import json
 from tqdm import tqdm
-
-max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
-dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
-load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
-
-model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name = "lora_model", # YOUR MODEL YOU USED FOR TRAINING
-        max_seq_length = max_seq_length,
-        dtype = dtype,
-        load_in_4bit = load_in_4bit,
-    )
 
 alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
@@ -26,56 +16,67 @@ alpaca_prompt = """Below is an instruction that describes a task, paired with an
 ### Response:
 {}"""
 
-from transformers import TextStreamer
-def query(instruction, user_input):
-    FastLanguageModel.for_inference(model) # Enable native 2x faster inference
-    inputs = tokenizer(
-    [
-        alpaca_prompt.format(
-            instruction, # instruction
-            user_input, # input
-            "", # output - leave this blank for generation!
-        )
-    ], return_tensors = "pt").to("cuda")
+class Evaluator:
+    def __init__(self, model, tokenizer):
+        self.model = model
+        self.tokenizer = tokenizer
 
-    # text_streamer = TextStreamer(tokenizer)
-    # _ = model.generate(**inputs, streamer = text_streamer, max_new_tokens = 2048)
+    def inference(self, instruction, user_input, stream=False):
+        FastLanguageModel.for_inference(self.model) # Enable native 2x faster inference
+        inputs = self.tokenizer(
+        [
+            alpaca_prompt.format(
+                instruction, # instruction
+                user_input, # input
+                "", # output - leave this blank for generation!
+            )
+        ], return_tensors = "pt").to("cuda")
 
-    output = model.generate(**inputs, max_length = 2048, do_sample = True, top_p = 0.9, top_k = 50, temperature = 1.0, num_return_sequences = 1)
-    output = tokenizer.batch_decode(output, skip_special_tokens = True)[0]
-    return output
+        if stream:
+            text_streamer = TextStreamer(self.tokenizer)
+            output = self.model.generate(**inputs, streamer = text_streamer, max_new_tokens = 2048)
 
-def metric(pred, gold):
-    TP = len(set(pred) & set(gold))
-    P = len(pred)
-    R = len(gold)
-    precision = TP / P if P > 0 else 0
-    recall = TP / R if R > 0 else 0
-    f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
-    return precision, recall, f1_score
+        else:
+            output = self.model.generate(**inputs, max_length = 2048, do_sample = True, top_p = 0.9, top_k = 50, temperature = 1.0, num_return_sequences = 1)
+            output = self.tokenizer.batch_decode(output, skip_special_tokens = True)[0]
+        return output
 
-def evaluate(test_file: str, wrong_output_file: str):
-    wrong_data = []
-    tot_precision, tot_recall, tot_f1_score = 0, 0, 0
-    with open(test_file, 'r') as f:
-        lines = f.readlines()
-        bar = tqdm(total=len(lines))
-        for i, line in enumerate(lines):
-            data = json.loads(line)
-            instruction = data['instruction']
-            input_ = data['input']
-            output = eval(query(instruction, input_).split('### Response:\n')[-1])
-            gold = data['output']
-            precision, recall, f1_score = metric(output, gold)
-            bar.update(1)
-            tot_precision += precision
-            tot_recall += recall
-            tot_f1_score += f1_score
-            bar.set_description(f"P: {tot_precision/(i+1):.4f}, R: {tot_recall/(i+1):.4f}, F1: {tot_f1_score/(i+1):.4f}")
-            if output!= gold:
-                wrong_data.append(data)
-        bar.close()
+    def metric(self, pred, gold):
+        TP = len(set(pred) & set(gold))
+        P = len(pred)
+        R = len(gold)
+        precision = TP / P if P > 0 else 0
+        recall = TP / R if R > 0 else 0
+        f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+        return precision, recall, f1_score
 
-    with open(wrong_output_file, 'w', encoding='utf-8') as f:
-        for data in wrong_data:
-            f.write(json.dumps(data) + '\n')
+    def evaluate(self, test_file: str, wrong_output_path: str = ''):
+        wrong_data = []
+        tot_precision, tot_recall, tot_f1_score = 0, 0, 0
+        with open(test_file, 'r') as f:
+            lines = f.readlines()
+            bar = tqdm(total=len(lines))
+            for i, line in enumerate(lines):
+                data = json.loads(line)
+                instruction = data['instruction']
+                input_ = data['input']
+                output = eval(self.inference(instruction, input_).split('### Response:\n')[-1])
+                gold = data['output']
+                precision, recall, f1_score = self.metric(output, gold)
+                bar.update(1)
+                tot_precision += precision
+                tot_recall += recall
+                tot_f1_score += f1_score
+                bar.set_description(f"P: {tot_precision/(i+1):.4f}, R: {tot_recall/(i+1):.4f}, F1: {tot_f1_score/(i+1):.4f}")
+                if output!= gold:
+                    wrong_data.append(data)
+            bar.close()
+
+        if wrong_output_path:
+            with open(wrong_output_path, 'w', encoding='utf-8') as f:
+                for data in wrong_data:
+                    f.write(json.dumps(data, ensure_ascii=False) + '\n')
+
+        return {'precision': tot_precision/len(lines), \
+                'recall': tot_recall/len(lines), \
+                'f1_score': tot_f1_score/len(lines)}
--- finetune.py
+++ finetune.py
@@ -1,111 +1,111 @@
 from dotenv import load_dotenv
 load_dotenv()
-# from unsloth import FastLanguageModel, is_bfloat16_supported
-# from datasets import load_dataset
-# from trl import SFTTrainer
-# from transformers import TrainingArguments
-# from evaluate import evaluate
+from unsloth import FastLanguageModel, is_bfloat16_supported
+from datasets import load_dataset
+from trl import SFTTrainer
+from transformers import TrainingArguments
+from evaluate import Evaluator
 from dataAug import DataAugmentation
 from prompt import lazy_prompt, implicit_prompt
 
-# max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
-# dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
-# load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
 
-# # 4bit pre quantized models we support for 4x faster downloading + no OOMs.
-# # See models at https://huggingface.co/unsloth
+# 4bit pre quantized models we support for 4x faster downloading + no OOMs.
+# See models at https://huggingface.co/unsloth
 
-# model, tokenizer = FastLanguageModel.from_pretrained(
-#     model_name = "unsloth/codellama-7b-bnb-4bit",
-#     max_seq_length = max_seq_length,
-#     dtype = dtype,
-#     load_in_4bit = load_in_4bit,
-#     # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
-# )
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "unsloth/mistral-7b-instruct-v0.3-bnb-4bit",
+    max_seq_length = max_seq_length,
+    dtype = dtype,
+    load_in_4bit = load_in_4bit,
+    # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+)
 
-# model = FastLanguageModel.get_peft_model(
-#     model,
-#     r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
-#     target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
-#                       "gate_proj", "up_proj", "down_proj",],
-#     lora_alpha = 16,
-#     lora_dropout = 0, # Supports any, but = 0 is optimized
-#     bias = "none",    # Supports any, but = "none" is optimized
-#     # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
-#     use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
-#     random_state = 3407,
-#     use_rslora = False,  # We support rank stabilized LoRA
-#     loftq_config = None, # And LoftQ
-# )
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 16,
+    lora_dropout = 0, # Supports any, but = 0 is optimized
+    bias = "none",    # Supports any, but = "none" is optimized
+    # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+    use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+    random_state = 3407,
+    use_rslora = False,  # We support rank stabilized LoRA
+    loftq_config = None, # And LoftQ
+)
 
-# alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
-# ### Instruction:
-# {}
+### Instruction:
+{}
 
-# ### Input:
-# {}
+### Input:
+{}
 
-# ### Response:
-# {}"""
+### Response:
+{}"""
 
-# EOS_TOKEN = tokenizer.eos_token # Must add EOS_TOKEN
-# def formatting_prompts_func(examples):
-#     instructions  = examples["instruction"]
-#     inputs       = examples["input"]
-#     outputs      = examples["output"]
-#     texts = []
-#     for instruction, input, output in zip(instructions, inputs, outputs):
-#         # Must add EOS_TOKEN, otherwise your generation will go on forever!
-#         text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN
-#         texts.append(text)
-#     return { "text" : texts, }
+EOS_TOKEN = tokenizer.eos_token # Must add EOS_TOKEN
+def formatting_prompts_func(examples):
+    instructions  = examples["instruction"]
+    inputs       = examples["input"]
+    outputs      = examples["output"]
+    texts = []
+    for instruction, input, output in zip(instructions, inputs, outputs):
+        # Must add EOS_TOKEN, otherwise your generation will go on forever!
+        text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN
+        texts.append(text)
+    return { "text" : texts, }
 
-# arguments = TrainingArguments(
-#     per_device_train_batch_size = 2,
-#     gradient_accumulation_steps = 4,
-#     warmup_steps = 5,
-#     # num_train_epochs = 1, # Set this for 1 full training run.
-#     max_steps = 40,
-#     learning_rate = 2e-4,
-#     fp16 = not is_bfloat16_supported(),
-#     bf16 = is_bfloat16_supported(),
-#     logging_steps = 1,
-#     optim = "adamw_8bit",
-#     weight_decay = 0.01,
-#     lr_scheduler_type = "linear",
-#     seed = 3407,
-#     output_dir = "outputs",
-# )
+arguments = TrainingArguments(
+    per_device_train_batch_size = 2,
+    gradient_accumulation_steps = 4,
+    warmup_steps = 5,
+    # num_train_epochs = 1, # Set this for 1 full training run.
+    max_steps = 40,
+    learning_rate = 2e-4,
+    fp16 = not is_bfloat16_supported(),
+    bf16 = is_bfloat16_supported(),
+    logging_steps = 1,
+    optim = "adamw_8bit",
+    weight_decay = 0.01,
+    lr_scheduler_type = "linear",
+    seed = 3407,
+    output_dir = "outputs",
+)
 
-# dataset = load_dataset('json', data_files='dataset/train.jsonl', split='train')
-# train_dataset = dataset.map(formatting_prompts_func, batched = True,)
+dataset = load_dataset('json', data_files='dataset/train.jsonl', split='train')
+train_dataset = dataset.map(formatting_prompts_func, batched = True,)
 
-# trainer = SFTTrainer(
-#     model = model,
-#     tokenizer = tokenizer,
-#     train_dataset = train_dataset,
-#     dataset_text_field = "text",
-#     max_seq_length = max_seq_length,
-#     dataset_num_proc = 1,
-#     packing = False, # Can make training 5x faster for short sequences.
-#     args = arguments
-# )
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = train_dataset,
+    dataset_text_field = "text",
+    max_seq_length = max_seq_length,
+    dataset_num_proc = 1,
+    packing = False, # Can make training 5x faster for short sequences.
+    args = arguments
+)
 
 for i in range(10):
-    # trainer_stats = trainer.train()
-    # model.save_pretrained("lora_model")
-    # tokenizer.save_pretrained("lora_model")
+    trainer_stats = trainer.train()
+    model.save_pretrained("lora_model")
+    tokenizer.save_pretrained("lora_model")
 
-    # evaluate(test_file='dataset/test.jsonl', wrong_output_file='dataset/wrong_data.jsonl')
+    evaluator = Evaluator(model, tokenizer)
+    result = evaluator.evaluate(test_file='dataset/test.jsonl', wrong_output_path='dataset/wrong_data.jsonl')
     
-    dataAug = DataAugmentation.from_file('dataset/wrong_data.jsonl')
-    augDataset = dataAug.augment(lazy_prompt, output_path='dataset/train.jsonl', from_log=False, repeat_num=1)
-    augDataset.log.set_zero()
+    with open('results.txt', 'a') as f:
+        f.write(f'Run {i+1}: {result}\n')
 
     dataAug = DataAugmentation.from_file('dataset/wrong_data.jsonl')
-    augDataset = dataAug.augment(implicit_prompt, output_path='dataset/train.jsonl', from_log=False, repeat_num=1)
-    augDataset.log.set_zero()
+    augDataset = dataAug.augment(lazy_prompt, output_path='dataset/train.jsonl', from_log=False, repeat_num=3)
+    augDataset = dataAug.augment(implicit_prompt, output_path='dataset/train.jsonl', from_log=False, repeat_num=3)
 
     dataset = load_dataset('json', data_files='dataset/train.jsonl', split='train')
     train_dataset = dataset.map(formatting_prompts_func, batched = True,)
